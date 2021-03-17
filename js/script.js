e115629836bfd04aa0ca1dc70401b4208250e46c
Bryan Douglas Lima Dos Santos <bryandouglasxd@gmail.com>
2021-03-16 23:39:01 -0300

    Update 1 JS

---
+++ js/script.js
@@ -0,0 +1,162 @@
+const Modal = {
+  open() {
+    // Abrir modal
+    //Adicionar a class active ao modal
+    document.querySelector('.modal-overlay').classList.add('active');
+  },
+  close() {
+    //Fechar modal
+    // Remover a class active do modal
+    document.querySelector('.modal-overlay').classList.remove('active');
+  },
+};
+
+const transactions = [
+  {
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  {
+    description: 'website',
+    amount: 50000,
+    date: '23/01/2021',
+  },
+  {
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  },
+];
+
+const Transaction = {
+  all: transactions,
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+
+  incomes() {
+    let income = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+
+    return income;
+  },
+
+  expenses() {
+    let expense = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+
+    return expense;
+  },
+
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
+
+const DOM = {
+  transactionContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionContainer.appendChild(tr);
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
+
+    const amount = Utils.formartCurrency(transaction.amount);
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td>
+      <img src="./img/minus.svg" alt="Remover transação" />
+    </td>`;
+
+    return html;
+  },
+
+  updateBalance() {
+    document.getElementById('incomeDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById('expenseDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById('totalDisplay').innerHTML = Utils.formartCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearTransactions() {
+    DOM.transactionContainer.innerHTML = '';
+  },
+};
+
+const Utils = {
+  //formatação de dinheiro em BRL
+
+  formartCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : '';
+
+    value = String(value).replace(/\D/g, '');
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return signal + value;
+  },
+};
+
+const Form = {
+  formatData() {
+    console.log('Formatar os dados');
+  },
+  validateFields() {
+    console.log('validar os campos');
+  },
+  submit(event) {
+    event.preventDefault();
+    Form.validateFields();
+    Form.formatData();
+  },
+};
+
+const App = {
+  init() {
+    Transaction.all.forEach((transaction) => {
+      DOM.addTransaction(transaction);
+    });
+
+    DOM.updateBalance();
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
